2dc185f2a60b109034a8697a9523f51af120ca5b
PVI-Engineers <contact@pviengineers.com>
2026-06-05 15:22:01 +0530

    test 20

--- pvi-careers-api/src/main/java/com/pviengineers/careersapi/model/CareerApplication.java
+++ pvi-careers-api/src/main/java/com/pviengineers/careersapi/model/CareerApplication.java
@@ -1,6 +1,5 @@
 package com.pviengineers.careersapi.model;
 
-import jakarta.persistence.Basic;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,13 +7,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "career_applications")
@@ -120,8 +120,7 @@ public class CareerApplication {
     @Column(length = 700)
     private String resumeStorageKey;
 
-    @Lob
-    @Basic
+    @JdbcTypeCode(SqlTypes.VARBINARY)
     @Column(columnDefinition = "bytea")
     private byte[] resumeData;
 
